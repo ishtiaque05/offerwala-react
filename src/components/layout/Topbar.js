94b199b1f8b38bf91d6ee3858c3f88689aa246f9
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-10-16 12:03:10 +0600

    Initial commit

---
+++ src/components/layout/Topbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Topbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Topbar</h1>
+      </div>
+    );
+  }
+}
+
+export default Topbar;
