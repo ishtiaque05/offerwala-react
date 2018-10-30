40e2f5400eea35600e1e117c257ee61f71a0af15
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-10-30 13:53:17 +0600

    created the `App` component and ran the first test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+
+describe('App', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  it('renders properly', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
