aa90ec3aee8daf42e57710faa07f966c6a81a795
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-11-13 15:40:28 +0600

    added chrome redux devtools integration

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from 'reducers';
 
 export default ({ children }) => {
-  const store = createStore(reducers, {}, applyMiddleware(thunk));
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(
+    reducers,
+    {},
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
   return <Provider store={store}>{children}</Provider>;
 };
