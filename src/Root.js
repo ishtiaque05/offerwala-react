c01aefaaff2c4a77f17838de22d07ae7890288cf
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-12-26 23:40:00 +0600

    reverted merge cra-universal

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 export default ({ children }) => {
-  return (
-    <Provider store={store}>
-      <Fragment>{children}</Fragment>
-    </Provider>
+  const middleware = [thunk];
+  const store = createStore(
+    reducers,
+    {},
+    composeWithDevTools(applyMiddleware(...middleware))
   );
+
+  return <Provider store={store}>{children}</Provider>;
 };
