eb08aa37d82dde6a1cdef55d1ce2651076adc9fe
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-12-28 18:00:19 +0600

    dist folder for heroku

---
+++ dist/client/precache-manifest.2f7f2484da3269afab067b6183dd44f0.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce0c9ae08840a0b43bccb9f5a86e155d",
+    "url": "/static/media/poppins-latin-400.ce0c9ae0.woff2"
+  },
+  {
+    "revision": "60aafb81cf7be2362585",
+    "url": "/static/css/main.1f0e3912.chunk.css"
+  },
+  {
+    "revision": "1250c5ebf29c488f329a5ade53a936e5",
+    "url": "/static/media/poppins-latin-900italic.1250c5eb.woff"
+  },
+  {
+    "revision": "ae107aa338391e4faee0",
+    "url": "/static/js/1.ae107aa3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09b2526ca9b1b95249d922b101f77c56",
+    "url": "/static/media/user.09b2526c.svg"
+  },
+  {
+    "revision": "41e0b682a255ec1aa787c8d6641ae5cc",
+    "url": "/static/media/wallet.41e0b682.svg"
+  },
+  {
+    "revision": "f407a842b4e2497092482e4dce216dd7",
+    "url": "/static/media/Slider_1.f407a842.png"
+  },
+  {
+    "revision": "d70d2ff629c822fc06ca98034ec89c63",
+    "url": "/static/media/Slider_2.d70d2ff6.png"
+  },
+  {
+    "revision": "2b665f2a19f4a077dbce7fc35cac093d",
+    "url": "/static/media/Slider_3.2b665f2a.png"
+  },
+  {
+    "revision": "88f56b7df830cfc245caf7690e33e8c4",
+    "url": "/static/media/default_deal.88f56b7d.jpg"
+  },
+  {
+    "revision": "0669601713c6f9c55ff91ee8da4e1f85",
+    "url": "/static/media/everything.06696017.svg"
+  },
+  {
+    "revision": "93f9435149bb995405b4b8e23603868a",
+    "url": "/static/media/beauty-fitness.93f94351.svg"
+  },
+  {
+    "revision": "8845dc9d57bde9ff2354c1687e25e68e",
+    "url": "/static/media/food-dining.8845dc9d.svg"
+  },
+  {
+    "revision": "f8ac91015075301ac3e94601fe84ba94",
+    "url": "/static/media/shopping.f8ac9101.svg"
+  },
+  {
+    "revision": "7f3cbb62ed9324ba55d5d54930ae1db4",
+    "url": "/static/media/services.7f3cbb62.svg"
+  },
+  {
+    "revision": "bf82058c2a0e23d8b76a1ea3c372ac5a",
+    "url": "/static/media/mobile-network.bf82058c.svg"
+  },
+  {
+    "revision": "84a5e38e490abfc0098f1fc943bc3d6f",
+    "url": "/static/media/electronics.84a5e38e.svg"
+  },
+  {
+    "revision": "1aba7c00cb6a8d902b177be34443447b",
+    "url": "/static/media/hotels-travels.1aba7c00.svg"
+  },
+  {
+    "revision": "bd58497ba70d6abf202642dedc374733",
+    "url": "/static/media/banks-cards.bd58497b.svg"
+  },
+  {
+    "revision": "413affb90353a16268abd7acbc531d1e",
+    "url": "/static/media/XMLID_15_.413affb9.png"
+  },
+  {
+    "revision": "1f99261a5204c0c64b246a1811a9b11d",
+    "url": "/static/media/XMLID_31_.1f99261a.png"
+  },
+  {
+    "revision": "cf303dfc1a67ec734a949ccd6b62d7a0",
+    "url": "/static/media/XMLID_47_.cf303dfc.png"
+  },
+  {
+    "revision": "38de408b3dd9437e2d48b003430bc98c",
+    "url": "/static/media/XMLID_63_.38de408b.png"
+  },
+  {
+    "revision": "4c37df809134795265ae4c7170af5e89",
+    "url": "/static/media/facebook.4c37df80.svg"
+  },
+  {
+    "revision": "50a21bc4c168752f1a570aaf2d80c39a",
+    "url": "/static/media/twitter.50a21bc4.svg"
+  },
+  {
+    "revision": "69c66ae9f79338437c7e23a4538d8e8a",
+    "url": "/static/media/youtube.69c66ae9.svg"
+  },
+  {
+    "revision": "838c9c3ce0847e4966c318421e1c8676",
+    "url": "/static/media/insta.838c9c3c.svg"
+  },
+  {
+    "revision": "4b31ef269911c1a586d13f31dc4cd57c",
+    "url": "/static/media/partner.4b31ef26.svg"
+  },
+  {
+    "revision": "652e34468737efcea6e3c81baaac31fa",
+    "url": "/static/media/Side_Ad–17.652e3446.png"
+  },
+  {
+    "revision": "f4ed853682a1ca4d3c5147265872a538",
+    "url": "/static/media/poppins-latin-100.f4ed8536.woff2"
+  },
+  {
+    "revision": "fba994d4045c23721ed05504854a5357",
+    "url": "/static/media/poppins-latin-200.fba994d4.woff2"
+  },
+  {
+    "revision": "f6ef809fb833dbf1abcd5b35ab1576e5",
+    "url": "/static/media/poppins-latin-300.f6ef809f.woff2"
+  },
+  {
+    "revision": "60aafb81cf7be2362585",
+    "url": "/static/js/main.60aafb81.chunk.js"
+  },
+  {
+    "revision": "08609a017d830988630ee1b38a7ef71a",
+    "url": "/static/media/poppins-latin-500.08609a01.woff2"
+  },
+  {
+    "revision": "d01ac68c4747e41a359cd94cecc29f9f",
+    "url": "/static/media/poppins-latin-600.d01ac68c.woff2"
+  },
+  {
+    "revision": "d9f0e4b31953d6440559059230442358",
+    "url": "/static/media/poppins-latin-700.d9f0e4b3.woff2"
+  },
+  {
+    "revision": "6f22cd6faf712f6973ee0f38cd38c0e6",
+    "url": "/static/media/poppins-latin-800.6f22cd6f.woff2"
+  },
+  {
+    "revision": "f53d38d7a9ce9fb32f278410609cd7d4",
+    "url": "/static/media/poppins-latin-900.f53d38d7.woff2"
+  },
+  {
+    "revision": "7f5b902dea02c28dc1183d7f623aa0fb",
+    "url": "/static/media/poppins-latin-100.7f5b902d.woff"
+  },
+  {
+    "revision": "ee845b5b261939c73e21ae3bf299aaf8",
+    "url": "/static/media/poppins-latin-100italic.ee845b5b.woff2"
+  },
+  {
+    "revision": "3ed2510a3f1ba88e0dfc61cb1966b4b4",
+    "url": "/static/media/poppins-latin-100italic.3ed2510a.woff"
+  },
+  {
+    "revision": "d7ef15c0b436fac613b1900fab84c93f",
+    "url": "/static/media/poppins-latin-200.d7ef15c0.woff"
+  },
+  {
+    "revision": "0fb363df3d0a40b613b9332d44b1d2c2",
+    "url": "/static/media/poppins-latin-200italic.0fb363df.woff2"
+  },
+  {
+    "revision": "9626a4b716b0fabc582171a7db38b0b5",
+    "url": "/static/media/poppins-latin-200italic.9626a4b7.woff"
+  },
+  {
+    "revision": "dad8b32d6402d45efbd9f2a8ee6f203a",
+    "url": "/static/media/poppins-latin-300.dad8b32d.woff"
+  },
+  {
+    "revision": "5e4a0e72a17dffda2e8c2af3af358110",
+    "url": "/static/media/poppins-latin-300italic.5e4a0e72.woff2"
+  },
+  {
+    "revision": "ffcf1f1c66a6661dabcf9a52581186d5",
+    "url": "/static/media/poppins-latin-300italic.ffcf1f1c.woff"
+  },
+  {
+    "revision": "1fce830e6112511a77108832e13172fd",
+    "url": "/static/media/poppins-latin-400.1fce830e.woff"
+  },
+  {
+    "revision": "936aaefc2efb764f3f52c0e9f34ef749",
+    "url": "/static/media/poppins-latin-400italic.936aaefc.woff2"
+  },
+  {
+    "revision": "055f0699f59e8b1eb28270b304c5d11b",
+    "url": "/static/media/poppins-latin-400italic.055f0699.woff"
+  },
+  {
+    "revision": "0261e08bd22d9f91c1d277cd4874ec95",
+    "url": "/static/media/poppins-latin-500.0261e08b.woff"
+  },
+  {
+    "revision": "a43a4a36fcfe4d073146b54490eec662",
+    "url": "/static/media/poppins-latin-500italic.a43a4a36.woff2"
+  },
+  {
+    "revision": "fabb1cda13707046efa0f7238555cc01",
+    "url": "/static/media/poppins-latin-500italic.fabb1cda.woff"
+  },
+  {
+    "revision": "02faed9f2938bbba57514dfbea590d56",
+    "url": "/static/media/poppins-latin-600.02faed9f.woff"
+  },
+  {
+    "revision": "fa071664f519e9e9409d8a6614b7c854",
+    "url": "/static/media/poppins-latin-600italic.fa071664.woff2"
+  },
+  {
+    "revision": "4624d18c5dd4c89fa2055339db8641ce",
+    "url": "/static/media/poppins-latin-600italic.4624d18c.woff"
+  },
+  {
+    "revision": "da80a603d0220f8e5bb3aaf0e5b42763",
+    "url": "/static/media/poppins-latin-700italic.da80a603.woff2"
+  },
+  {
+    "revision": "d1d7b85dc76bad1a967fc842d110262c",
+    "url": "/static/media/poppins-latin-700.d1d7b85d.woff"
+  },
+  {
+    "revision": "d03f7611353f6f9e728b329d982b7660",
+    "url": "/static/media/poppins-latin-700italic.d03f7611.woff"
+  },
+  {
+    "revision": "99a9e7bac31ddfc2b58e0025f76c65d1",
+    "url": "/static/media/poppins-latin-800.99a9e7ba.woff"
+  },
+  {
+    "revision": "5c8a310a6164fe0dfec074a89d94f181",
+    "url": "/static/media/poppins-latin-800italic.5c8a310a.woff"
+  },
+  {
+    "revision": "acbf8d91de92514c5111f02cb26854a9",
+    "url": "/static/media/poppins-latin-800italic.acbf8d91.woff2"
+  },
+  {
+    "revision": "1b8eddbe62c11ea9eb4f407ecd897827",
+    "url": "/static/media/poppins-latin-900.1b8eddbe.woff"
+  },
+  {
+    "revision": "470d942f71759896d116a87d770a8c2e",
+    "url": "/static/media/poppins-latin-900italic.470d942f.woff2"
+  },
+  {
+    "revision": "ae107aa338391e4faee0",
+    "url": "/static/css/1.4e854b53.chunk.css"
+  },
+  {
+    "revision": "fce15f8fae3bb2de00fcf78ce06cee03",
+    "url": "/index.html"
+  }
+];
